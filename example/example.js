787c77c730ea575aacc517f4e199c67c480dd9ed
Quentin Presley <qpresley@ca.ibm.com>
2016-05-17 08:15:08 -0700

    Initial commit

---
+++ example/example.js
@@ -0,0 +1,42 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-db2
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+var DataSource = require('loopback-datasource-juggler').DataSource;
+var DASHDB = require('../'); // loopback-connector-dashdb
+
+var config = {
+  username: process.env.DASHDB_USERNAME,
+  password: process.env.DASHDB_PASSWORD,
+  hostname: process.env.DASHDB_HOSTNAME,
+  port: 50000,
+  database: 'SQLDB',
+};
+
+var db = new DataSource(DASHDB, config);
+
+var User = db.define('User', {name: {type: String}, email: {type: String},
+});
+
+db.autoupdate('User', function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  User.create({
+    name: 'Tony',
+    email: 'tony@t.com',
+  }, function(err, user) {
+    console.log(err, user);
+  });
+
+  User.find({where: {name: 'Tony'}}, function(err, users) {
+    console.log(err, users);
+  });
+
+  User.destroyAll(function() {
+    console.log('example complete');
+  });
+});
---
+++ test/imported.test.js
@@ -0,0 +1,17 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-db2
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+/* eslint-env node, mocha */
+describe('db2 imported features', function() {
+
+  before(function() {
+    require('./init.js');
+  });
+
+  require('loopback-datasource-juggler/test/common.batch.js');
+  require('loopback-datasource-juggler/test/include.test.js');
+  require('loopback-connector-db2/test/db2.connection.test.js');
+
+});
