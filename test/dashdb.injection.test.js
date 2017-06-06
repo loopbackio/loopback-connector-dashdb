7f85b8e26e6ec0d30799e7b7118374fb7cfd93c1
Janny <houjueforwork@gmail.com>
2017-06-06 10:05:20 -0400

    ParameterizeSQL (#48)
    * ParameterizeSQL

---
+++ test/dashdb.injection.test.js
@@ -0,0 +1,44 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: loopback-connector-dashdb
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+'use strict';
+
+/* eslint-env node, mocha */
+process.env.NODE_ENV = 'test';
+
+require('./init.js');
+require('should');
+
+var db, Customer;
+
+describe('parameterized SQL', function() {
+  before(function(done) {
+    db = global.getDataSource();
+    Customer = db.define('Customer', {
+      name: {type: String},
+      pass: {type: String},
+    });
+    db.automigrate('Customer', function(err) {
+      if (err) return done(err);
+      var samples = [
+          {name: 'Zoe', pass: '123'},
+          {name: 'John', pass: '456'},
+      ];
+      Customer.create(samples, done);
+    });
+  });
+  it('does not return result with SQL injection', function(done) {
+    // generate injectedSQL as `SELECT * FROM Customer WHERE name ="" or 1=1`
+    // 1=1 is always true
+    var injectedFilter = {
+      where: {or: [{name: ''}, {1: 1}]},
+    };
+    Customer.find(injectedFilter, function(err, result) {
+      if (err) return done(err);
+      result.length.should.equal(0);
+      done();
+    });
+  });
+});
